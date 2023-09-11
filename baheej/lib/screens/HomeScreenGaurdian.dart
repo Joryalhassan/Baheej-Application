d84a26ae4fb4780693f052ce7da5a7dfa28c615d
renadalnumay <94903216+renadalnumay@users.noreply.github.com>
2023-09-11 22:11:24 +0300

    switch branch

--- baheej/lib/screens/HomeScreenGaurdian.dart
+++ baheej/lib/screens/HomeScreenGaurdian.dart
@@ -1,12 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-//<<<<<<< HEAD:baheej/lib/screens/home-page.dart
-import 'package:baheej/screens/Service.dart';
-import 'package:baheej/screens/ServiceFormScreen.dart';
-import 'package:baheej/screens/Service.dart';
-//=======
 import 'package:baheej/screens/WelcomePage.dart';
-//>>>>>>> origin/main:baheej/lib/screens/HomeScreenGaurdian.dart
 
 class HomeScreenGaurdian extends StatefulWidget {
   const HomeScreenGaurdian({Key? key}) : super(key: key);
@@ -15,28 +9,6 @@ class HomeScreenGaurdian extends StatefulWidget {
   _HomeScreenGaurdianState createState() => _HomeScreenGaurdianState();
 }
 
-<<<<<<< HEAD:baheej/lib/screens/home-page.dart
-class _HomeScreenState extends State<HomeScreen> {
-  List<Service> services = [];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Screen'),
-        backgroundColor: Color.fromARGB(255, 98, 144, 224),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.logout), // Add the logout icon here
-            onPressed: () {
-              FirebaseAuth.instance.signOut().then((value) {
-                print("Signed Out");
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => SignInScreen()));
-              });
-            },
-          ),
-        ],
-////=======
 class _HomeScreenGaurdianState extends State<HomeScreenGaurdian> {
   @override
   Widget build(BuildContext context) {
@@ -48,120 +20,112 @@ class _HomeScreenGaurdianState extends State<HomeScreenGaurdian> {
             FirebaseAuth.instance.signOut().then((value) {
               print("Signed Out");
               Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => WelcomePage())); 
+                  MaterialPageRoute(builder: (context) => WelcomePage()));
             });
           },
         ),
-//>>>>>>> origin/main:baheej/lib/screens/HomeScreenGaurdian.dart
       ),
-      body: ListView.builder(
-        itemCount: services.length,
-        itemBuilder: (context, index) {
-          final service = services[index];
-          return Card(
-            color: Color.fromARGB(255, 243, 243, 244),
-            elevation: 1,
-            margin: EdgeInsets.all(20),
-            child: ListTile(
-              title: Text(service.name),
-              textColor: Color.fromARGB(255, 74, 72, 85),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Time: ${service.time.format(context)}'),
-                  Text('Price: \$${service.price.toStringAsFixed(2)}'),
-                  Text('Description: ${service.description}'),
-                  Text('Capacity: ${service.capacity}'),
-                  Text('Age: ${service.age}+'),
-                ],
-              ),
-            ),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-          onPressed: () async {
-            final newService = await Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => ServiceFormScreen(),
-              ),
-            );
-
-            if (newService != null) {
-              setState(() {
-                services.add(newService);
-              });
-            }
-          },
-          child: Icon(Icons.add),
-          backgroundColor: Color.fromARGB(255, 98, 144, 224)),
     );
   }
 }
-  // List<Service> services = [];
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     body: Center(
-  //       child: ElevatedButton(
-  //         child: Text("Logout"),
-  //         onPressed: () {
-  //           FirebaseAuth.instance.signOut().then((value) {
-  //             print("Signed Out");
-  //             Navigator.push(context,
-  //                 MaterialPageRoute(builder: (context) => SignInScreen()));
-  //           });
-  //         },
-  //       ),
-  //     ),
-  //   );
-  // }
 
-
-
-
-// import 'package:flutter/material.dart';
 // import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:baheej/screens/auth.dart';
+// import 'package:flutter/material.dart';
+// //<<<<<<< HEAD:baheej/lib/screens/home-page.dart
+// import 'package:baheej/screens/Service.dart';
+// import 'package:baheej/screens/ServiceFormScreen.dart';
+// import 'package:baheej/screens/Service.dart';
+// import 'package:baheej/screens/WelcomePage.dart';
+// import 'package:baheej/screens/HomeScreenGaurdian.dart';
 
-// class HomePage extends StatelessWidget {
-//   final User? user = Auth().currentUser; // Declare the type explicitly
+// class HomeScreenGaurdian extends StatefulWidget {
+//   const HomeScreenGaurdian({Key? key}) : super(key: key);
 
-//   HomePage({Key? key}) : super(key: key);
+//   @override
+//   _HomeScreenGaurdianState createState() => _HomeScreenGaurdianState();
+// }
 
-//   Future<void> signout() async {
-//     await Auth().signout();
-//   }
-
-//   Widget _title() {
-//     return const Text("firebase Auth");
-//   }
-
-//   Widget _ueserUid() {
-//     return Text(user?.email ?? 'User email');
-//   }
-
-//   Widget _signOutButton() {
-//     return ElevatedButton(onPressed: signout, child: const Text('signout'));
-//   }
-
+// //<<<<<<< HEAD:baheej/lib/screens/home-page.dart
+// // class _HomeScreenGaurdianState extends State<HomeScreenGaurdian> {
+// //   List<Service> services = [];
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return Scaffold(
+// //       appBar: AppBar(
+// //         title: Text('Home Screen'),
+// //         backgroundColor: Color.fromARGB(255, 98, 144, 224),
+// //         actions: [
+// //           IconButton(
+// //             icon: Icon(Icons.logout), // Add the logout icon here
+// //             onPressed: () {
+// //               FirebaseAuth.instance.signOut().then((value) {
+// //                 print("Signed Out");
+// //                 Navigator.push(context,
+// //                     MaterialPageRoute(builder: (context) => WelcomePage()));
+// //               });
+// //             },
+// //           ),
+// //         ],
+// ////=======
+// class _HomeScreenGaurdianState extends State<HomeScreenGaurdian> {
 //   @override
 //   Widget build(BuildContext context) {
 //     return Scaffold(
-//       appBar: AppBar(
-//         title: _title(),
-//       ),
-//       body: Container(
-//         height: double.infinity,
-//         width: double.infinity,
-//         padding: const EdgeInsets.all(20),
-//         child: Column(
-//           crossAxisAlignment: CrossAxisAlignment.center,
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[],
+//       body: Center(
+//         child: ElevatedButton(
+//           child: Text("Logout"),
+//           onPressed: () {
+//             FirebaseAuth.instance.signOut().then((value) {
+//               print("Signed Out");
+//               Navigator.push(context,
+//                   MaterialPageRoute(builder: (context) => WelcomePage()));
+//             });
+//           },
 //         ),
 //       ),
+//       body: ListView.builder(
+//         itemCount: services.length,
+//         itemBuilder: (context, index) {
+//           final service = services[index];
+//           return Card(
+//             color: Color.fromARGB(255, 243, 243, 244),
+//             elevation: 1,
+//             margin: EdgeInsets.all(20),
+//             child: ListTile(
+//               title: Text(service.name),
+//               textColor: Color.fromARGB(255, 74, 72, 85),
+//               subtitle: Column(
+//                 crossAxisAlignment: CrossAxisAlignment.start,
+//                 children: [
+//                   Text('Time: ${service.time.format(context)}'),
+//                   Text('Price: \$${service.price.toStringAsFixed(2)}'),
+//                   Text('Description: ${service.description}'),
+//                   Text('Capacity: ${service.capacity}'),
+//                   Text('Age: ${service.age}+'),
+//                 ],
+//               ),
+//             ),
+//           );
+//         },
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//           onPressed: () async {
+//             final newService = await Navigator.push(
+//               context,
+//               MaterialPageRoute(
+//                 builder: (context) => ServiceFormScreen(),
+//               ),
+//             );
+
+//             if (newService != null) {
+//               setState(() {
+//                 services.add(newService);
+//               });
+//             }
+//           },
+//           child: Icon(Icons.add),
+//           backgroundColor: Color.fromARGB(255, 98, 144, 224)),
 //     );
 //   }
 // }
+  
