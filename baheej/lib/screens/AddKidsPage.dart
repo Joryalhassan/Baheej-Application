9dc207f6c49a0af2770131c034671f34425ba51b
najdaln <121226682+najdaln@users.noreply.github.com>
2023-10-02 21:31:37 +0300

    test

--- baheej/lib/screens/AddKidsPage.dart
+++ baheej/lib/screens/AddKidsPage.dart
@@ -9,157 +9,47 @@ class AddKidsPage extends StatefulWidget {
 
 class _AddKidsPageState extends State<AddKidsPage> {
   TextEditingController nameController = TextEditingController();
-  int age = 2; // Initialize age with 0
-  String? errorText; // Error text for age validation
-
-  String? _validateName(String? value) {
-    if (value == null || value.isEmpty) {
-      return 'Name is required';
-    }
-
-    if (value.length < 4 || value.length > 25) {
-      return 'Name must be between 4 and 25 letters';
-    }
-
-    if (!RegExp(r'^[a-zA-Z\s]+$').hasMatch(value)) {
-      return 'Name should only contain letters';
-    }
-
-    return null;
-  }
-
-  String? _validateAge(int value) {
-    if (value < 2 || value > 12) {
-      return 'Age must be between 2 and 12';
-    }
-
-    return null;
-  }
+  TextEditingController ageController = TextEditingController();
+  List<String> addedKidNames = []; // Maintain a list of added kid names
 
   Future<void> _addKidToFirestore(String name, int age) async {
     try {
-      final kidCollection = FirebaseFirestore.instance.collection('Kids');
+      if (age >= 2 && age <= 12) {
+        final kidCollection = FirebaseFirestore.instance.collection('Kids');
+        await kidCollection.add({
+          'name': name,
+          'age': age,
+        });
 
-      // Check if a kid with the same name already exists
-      final querySnapshot =
-          await kidCollection.where('name', isEqualTo: name).get();
+        // Add the name to the list of added kid names
+        addedKidNames.add(name);
 
-      if (querySnapshot.docs.isNotEmpty) {
-        // Kid with the same name already exists
+        // Show a success message
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
-            content: Text('A kid with this name already exists.'),
+            content: Text('Kid "$name" added successfully!'),
+            backgroundColor: Colors.green,
           ),
         );
       } else {
-        // Kid with the same name doesn't exist, proceed to add
-
-        // Validate age before adding
-        final ageValidationMessage = _validateAge(age);
-        if (ageValidationMessage != null) {
-          setState(() {
-            errorText = ageValidationMessage;
-          });
-        } else {
-          // Reset the error text if age is valid
-          setState(() {
-            errorText = null;
-          });
-
-          await kidCollection.add({
-            'name': name,
-            'age': age,
-          });
-
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content: Text('Kid added successfully.'),
-            ),
-          );
-
-          // Clear the name field and reset age
-          nameController.clear();
-          setState(() {
-            age = 0;
-          });
-        }
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Age must be between 2 and 12.'),
+            backgroundColor: Colors.red,
+          ),
+        );
       }
     } catch (e) {
       print('Error adding kid to Firestore: $e');
     }
   }
 
-  Widget buildIncrementDecrementField(
-    String label,
-    int value,
-    void Function() onIncrement,
-    void Function() onDecrement,
-    String? Function(int value)? validator, // Add a validator parameter
-  ) {
-    return Container(
-      margin: EdgeInsets.only(bottom: 8),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            label,
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 4),
-          Row(
-            children: [
-              IconButton(
-                icon: Icon(Icons.remove),
-                onPressed: () {
-                  setState(() {
-                    onDecrement();
-                  });
-                },
-              ),
-              Container(
-                width: 100,
-                child: TextFormField(
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.grey[300],
-                    contentPadding: EdgeInsets.symmetric(
-                      vertical: 8,
-                      horizontal: 12,
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12.0),
-                      borderSide: BorderSide(color: Colors.transparent),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12.0),
-                      borderSide: BorderSide(color: Colors.transparent),
-                    ),
-                    errorText: validator != null
-                        ? validator(value)
-                        : null, // Use the validator
-                  ),
-                  // Value is now controlled by the value variable
-                  controller: TextEditingController(text: value.toString()),
-                  readOnly: true, // Make the field read-only
-                ),
-              ),
-              IconButton(
-                icon: Icon(Icons.add),
-                onPressed: () {
-                  setState(() {
-                    onIncrement();
-                  });
-                },
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
+  bool isNameValid(String name) {
+    return name.length >= 4 && name.length <= 20;
+  }
+
+  bool containsOnlyLetters(String name) {
+    return RegExp(r'^[a-zA-Z]+$').hasMatch(name);
   }
 
   @override
@@ -204,95 +94,119 @@ class _AddKidsPageState extends State<AddKidsPage> {
                   children: <Widget>[
                     TextField(
                       controller: nameController,
-                      decoration: InputDecoration(
-                        labelText: 'Name',
-                        counterText: '', // Remove the character count
-                      ),
-                      maxLength: 25, // Set the maximum length to 25 characters
+                      decoration: InputDecoration(labelText: 'Name'),
+                      maxLength: 20, // Limit to 20 characters
                     ),
-                    buildIncrementDecrementField(
-                      'Age',
-                      age,
-                      () {
-                        // Increment age
-                        setState(() {
-                          if (age < 12) {
-                            age++;
-                          }
-                        });
-                      },
-                      () {
-                        // Decrement age (ensure it doesn't go below 2)
-                        setState(() {
-                          if (age > 2) {
-                            age--;
-                          }
-                        });
-                      },
-                      (value) {
-                        // Validator function for age
-                        if (value < 2 || value > 12) {
-                          return 'Age must be between 2 and 12';
-                        }
-                        return null; // No error if it's within the valid range
-                      },
+                    TextField(
+                      controller: ageController,
+                      decoration: InputDecoration(labelText: 'Age'),
+                      keyboardType: TextInputType.number,
+                      inputFormatters: [
+                        FilteringTextInputFormatter.digitsOnly,
+                        LengthLimitingTextInputFormatter(
+                            2), // Limit to 2 characters
+                      ],
                     ),
                   ],
                 ),
                 actions: <Widget>[
                   ElevatedButton(
                     style: ElevatedButton.styleFrom(
-                      primary: Colors
-                          .red, // Set the button's background color to red
+                      primary: Colors.red,
                       textStyle: TextStyle(
-                        fontSize: 16, // Set the font size
-                        fontWeight: FontWeight.bold, // Set the font weight
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
                       ),
                       padding: EdgeInsets.symmetric(
                         horizontal: 20,
                         vertical: 12,
-                      ), // Set padding
+                      ),
                       shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(
-                          12.0,
-                        ), // Set button's border radius
+                        borderRadius: BorderRadius.circular(12.0),
                       ),
                     ),
-                    child: Text('Cancel'), // Text for the Cancel button
+                    child: Text('Cancel'),
                     onPressed: () {
-                      Navigator.of(context)
-                          .pop(); // Close the dialog without saving
+                      Navigator.of(context).pop();
                     },
                   ),
                   ElevatedButton(
                     style: ElevatedButton.styleFrom(
-                      primary: Colors
-                          .blue, // Set the button's background color to blue
+                      primary: Colors.blue,
                       textStyle: TextStyle(
-                        fontSize: 16, // Set the font size
-                        fontWeight: FontWeight.bold, // Set the font weight
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
                       ),
                       padding: EdgeInsets.symmetric(
                         horizontal: 20,
                         vertical: 12,
-                      ), // Set padding
+                      ),
                       shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(
-                          12.0,
-                        ), // Set button's border radius
+                        borderRadius: BorderRadius.circular(12.0),
                       ),
                     ),
-                    child: Text('Done'), // Text for the Done button
+                    child: Text('Done'),
                     onPressed: () async {
                       final name = nameController.text;
-                      final ageValidationMessage = _validateAge(age);
-                      final nameValidationMessage = _validateName(name);
-                      if (name.isNotEmpty &&
-                          ageValidationMessage == null &&
-                          nameValidationMessage == null) {
+                      final ageStr = ageController.text;
+
+                      // Check for empty fields first
+                      if (name.isEmpty || ageStr.isEmpty) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text('Name and age can\'t be empty.'),
+                            backgroundColor: Colors.red,
+                          ),
+                        );
+                        return;
+                      }
+
+                      // Validate name length
+                      if (!isNameValid(name)) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text(
+                                'Name must be between 4 and 20 characters.'),
+                            backgroundColor: Colors.red,
+                          ),
+                        );
+                        return;
+                      }
+
+                      // Validate name contains only letters
+                      if (!containsOnlyLetters(name)) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text('Name must contain only letters.'),
+                            backgroundColor: Colors.red,
+                          ),
+                        );
+                        return;
+                      }
+
+                      if (addedKidNames.contains(name)) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content:
+                                Text('Kid with the same name already exists.'),
+                            backgroundColor: Colors.red,
+                          ),
+                        );
+                        return;
+                      }
+
+                      final age = int.tryParse(ageStr);
+                      if (age != null && age >= 2 && age <= 12) {
                         await _addKidToFirestore(name, age);
-                        Navigator.of(context)
-                            .pop(); // Close the dialog after saving
+                        Navigator.of(context).pop();
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text(
+                                'Invalid age input. Age must be between 2 and 12.'),
+                            backgroundColor: Colors.red,
+                          ),
+                        );
                       }
                     },
                   ),
