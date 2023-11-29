af8d0bdbd452e9e2b5edafeaeb12a70efbc0d7a7
Nada <nadaalqabbani@gmail.com>
2023-11-29 04:14:37 +0300

    button fixing

--- baheej/lib/screens/star_rating.dart
+++ baheej/lib/screens/star_rating.dart
@@ -30,6 +30,7 @@ class _StarRatingState extends State<StarRating> {
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Row(
           mainAxisSize: MainAxisSize.min,
@@ -52,75 +53,94 @@ class _StarRatingState extends State<StarRating> {
           ),
         ),
         SizedBox(height: 16),
-        ElevatedButton(
-          onPressed: canEditRating
-              ? () {
-                  // Show confirmation dialog
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title: Text('Confirm Rating'),
-                        content: Text('Do you want to save your rating?'),
-                        actions: <Widget>[
-                          TextButton(
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                            child: Text('Cancel'),
-                          ),
-                          TextButton(
-                            onPressed: () async {
-                              // Update Firestore with the new rating
-                              try {
-                                await FirebaseFirestore.instance
-                                    .collection('ServiceBook')
-                                    .doc(widget.serviceDocumentId)
-                                    .update({'starsrate': currentRating});
-
-                                widget.onRatingChanged(currentRating);
-
-                                // Close the confirmation dialog
+        Center(
+          child: ElevatedButton(
+            onPressed: canEditRating
+                ? () {
+                    // Show confirmation dialog
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text('Confirm Rating'),
+                          content: Text('Do you want to save your rating?'),
+                          actions: <Widget>[
+                            TextButton(
+                              onPressed: () {
                                 Navigator.of(context).pop();
+                              },
+                              child: Text('Cancel'),
+                            ),
+                            TextButton(
+                              onPressed: () async {
+                                // Update Firestore with the new rating
+                                try {
+                                  await FirebaseFirestore.instance
+                                      .collection('ServiceBook')
+                                      .doc(widget.serviceDocumentId)
+                                      .update({'starsrate': currentRating});
 
-                                // Disable further rating changes
-                                setState(() {
-                                  canEditRating = false;
-                                });
+                                  widget.onRatingChanged(currentRating);
 
-                                // Show a thank-you message
-                                showDialog(
-                                  context: context,
-                                  builder: (BuildContext context) {
-                                    return AlertDialog(
-                                      title: Text('Thank You!'),
-                                      content:
-                                          Text('Your feedback has been saved.'),
-                                      actions: <Widget>[
-                                        TextButton(
-                                          onPressed: () {
-                                            Navigator.of(context).pop();
-                                          },
-                                          child: Text('OK'),
-                                        ),
-                                      ],
-                                    );
-                                  },
-                                );
-                              } catch (e) {
-                                // Handle Firestore update error
-                                print('Error updating rating: $e');
-                              }
-                            },
-                            child: Text('Save'),
-                          ),
-                        ],
-                      );
-                    },
-                  );
-                }
-              : null,
-          child: Text('Submit Rating'),
+                                  // Close the confirmation dialog
+                                  Navigator.of(context).pop();
+
+                                  // Disable further rating changes
+                                  setState(() {
+                                    canEditRating = false;
+                                  });
+
+                                  // Show a thank-you message
+                                  showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return AlertDialog(
+                                        title: Text('Thank You!'),
+                                        content: Text(
+                                            'Your feedback has been saved.'),
+                                        actions: <Widget>[
+                                          TextButton(
+                                            onPressed: () {
+                                              Navigator.of(context).pop();
+                                            },
+                                            child: Text('OK'),
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  );
+                                } catch (e) {
+                                  // Handle Firestore update error
+                                  print('Error updating rating: $e');
+                                }
+                              },
+                              child: Text(
+                                'Save',
+                                style: TextStyle(
+                                    fontSize:
+                                        16.0), // Adjust the font size here
+                              ),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  }
+                : null,
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all<Color>(
+                  Color.fromARGB(255, 59, 138, 207)),
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+              ),
+            ),
+            child: Text(
+              'Submit Rating',
+              style: TextStyle(fontSize: 15.0), // Adjust the font size here
+            ),
+          ),
         ),
       ],
     );
